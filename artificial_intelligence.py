ff98a6e970eee95c3105d826e9bc064e1d77da82
Mike Edwards <michael.edwards.4108@gmail.com>
2021-05-17 19:06:12 -0400

    created class files required to operate virtual game

---
+++ artificial_intelligence.py
@@ -0,0 +1,6 @@
+from player import Player
+
+
+class AI(Player):
+    def __init__(self):
+        pass
---
+++ player_one.py
@@ -0,0 +1,6 @@
+from player import Player
+
+
+class PlayerOne(Player):
+    def __init__(self):
+        pass
