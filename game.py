fa9d0e5194a5cc0095fd4adfe3112fbed17a95f2
Mike Edwards <michael.edwards.4108@gmail.com>
2021-05-19 14:28:58 -0400

    completed choose_player_two method

--- game.py
+++ game.py
@@ -1,16 +1,18 @@
 import time
+from player import Player
+from human import Human
 from artificial_intelligence import AI
 
 
 class Game:
     def __init__(self):
-        self.player_one = ""
-        self.player_two = ""
+        self.player_one = Human()
+        self.player_two = Player()
 
     def run_game(self):
         time.sleep(1)
         print()
-        self.player_one = input("What is your name?")
+        self.player_one.name = input("What is your name?")
         print()
         print(f"Welcome {self.player_one}, to Rock, Paper, Scissors...Lizard, Spock!")
         print()
@@ -19,11 +21,14 @@ class Game:
         user_input = True
         while user_input:
             rule_check = input("Would you like to look at the rules?")
+            print()
+            print("*************************************************")
             if rule_check.lower() == "yes":
                 print()
                 self.game_rules()
             elif rule_check.lower() == "no":
                 print()
+                print("Now to select player two.")
                 self.choose_player_two()
             else:
                 print("Please enter yes or no.")
@@ -51,8 +56,11 @@ class Game:
         done_reading_rules = True
         while done_reading_rules:
             exit_rules = input("Are you ready to play the game?")
+            print()
+            print("*************************************************")
             if exit_rules.lower() == "yes":
                 print()
+                print("Now to select player two.")
                 self.choose_player_two()
             else:
                 continue
@@ -60,12 +68,37 @@ class Game:
     def choose_player_two(self):
         self.player_two = input("Select <1> to play against another human, or <2> to play against the computer.")
         if int(self.player_two) == 1:
-            self.player_two = input("What is player two's name?")
-            print(f"Time for {self.player_one} to play {self.player_two} in RPSLS!")
+            self.player_two = Human()
+            self.player_two.name = input("What is player two's name?")
+            print(f"Time for {self.player_one.name} to play {self.player_two.name} in RPSLS!")
+            print()
+            print("*************************************************")
+            print()
+            self.start_game()
+            return self.player_two.name
         elif int(self.player_two) == 2:
             self.player_two = AI()
-            print("You have selected to play against the computer.")
-        # self.start_rounds()
+            self.player_two.name = "Computer"
+            print(f"You have selected to play against the {self.player_two.name}.")
+            print("Let's begin!")
+            print()
+            print("*************************************************")
+            print()
+            self.start_game()
+            return self.player_two.name
 
-    # def start_rounds(self):
-    #     while self.player_one
+    def start_game(self):
+        while self.player_two.score < 3 or self.player_two.score < 3:
+            print(f"{self.player_one.name} goes first.")
+            self.human_vs_human_game_rounds()
+
+    # def human_vs_human_game_rounds(self):
+    #     self.player_two = Human()
+    #     self.player_one.sentient_gesture()
+    #     print(f"{self.player_one.name} made {self.player_one.chosen_gesture}.")
+    #     self.player_two.sentient_gesture()
+    #     print(f"{self.player_two.name} made {self.player_two.chosen_gesture}.")
+    #     if self.player_one.chosen_gesture == self.player_two.chosen_gesture:
+    #         print(f"Both players made {self.player_one.chosen_gesture}. Tie round.")
+    #     elif self.player_one.chosen_gesture == self.player_one.list_of_gestures[0]:
+    #         if self.player_two.chosen_gesture == self.player_two.list_of_gestures[1]:
