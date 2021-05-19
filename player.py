bf6940ec82a956c4953fb2e40a2260225286c327
Mike Edwards <michael.edwards.4108@gmail.com>
2021-05-19 16:39:38 -0400

    simplified player class-moved gesture_selection to child classes

--- player.py
+++ player.py
@@ -1,3 +1,6 @@
 class Player:
     def __init__(self):
-        pass
+        self.name = ""
+        self.list_of_gestures = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+        self.score = 0
+        self.chosen_gesture = None
