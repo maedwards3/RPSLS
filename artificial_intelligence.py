c285aa34e9bc21974eabe9a9827254d5446d0b3b
Mike Edwards <michael.edwards.4108@gmail.com>
2021-05-19 16:38:45 -0400

    created gesture_selection for computer player

--- artificial_intelligence.py
+++ artificial_intelligence.py
@@ -1,6 +1,10 @@
+import random
 from player import Player
 
 
 class AI(Player):
     def __init__(self):
-        pass
+        super().__init__()
+
+    def gesture_selection(self):
+        random.choice(self.list_of_gestures)
